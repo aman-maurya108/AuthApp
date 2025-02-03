30a959635f21d86b67a8689f4a3f43614ccecd2d
Aman Maurya <aman.20223030@mnnit.ac.in>
2025-02-03 19:05:38 +0530

    Updated changes

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,33 @@ const express = require("express");
 const router = express.Router();
 
 const {login,signup} = require("../controllers/Auth");
+const {auth , isStudent, isAdmin} = require("../middlewares/auth");
 
 router.post("/login",login);
 router.post("/signup",signup);
+
+//testing protected route for single middleware
+router.get("/test",auth,(req,res) =>{
+    res.json({
+        success : true,
+        message : "Welcome to the protected route for tests."
+    }); 
+});
+
+
+// protected route
+router.get("/student",auth,isStudent,(req,res) =>{
+    res.json({
+        success : true,
+        message : "Welcome to the protected route for students."
+    });
+});
+router.get("/admin",auth,isAdmin,(req,res) =>{
+    res.json({
+        success : true,
+        message : "Welcome to the protected route for admin."
+    });
+    
+});
 
 module.exports = router;
